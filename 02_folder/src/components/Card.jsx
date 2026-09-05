f776dcd1b77bdf5c3c789309ec6a00ab3a594745
Dev <sanjudayal999@gmail.com>
2026-09-05 18:33:45 +0530

    Completed components learning in 02_folder

---
+++ 02_folder/src/components/Card.jsx
@@ -0,0 +1,12 @@
+function Card() {
+  return (
+    <div className="card">
+      <h1>sanju </h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam, in.
+      </p>
+    </div>
+  );
+}
+
+export default Card;
