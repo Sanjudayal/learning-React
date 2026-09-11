edbe5e76ebbae80ca31cbee0527653eb40714ff7
Dev <sanjudayal999@gmail.com>
2026-09-11 19:03:25 +0530

    complete notes app project

--- 12_notes_app/src/App.jsx
+++ 12_notes_app/src/App.jsx
@@ -8,16 +8,24 @@ const App = () => {
 
   const submitHandler = (e) => {
     e.preventDefault();
+    if (heading === "" || note === "") {
+      alert("please first fill all the text areas then click to add note");
+    } else {
+      const newNoteArray = [...noteArray];
 
+      newNoteArray.push({ heading, note });
+
+      setNoteArray(newNoteArray);
+
+      setHeading("");
+      setNote("");
+    }
+  };
+
+  const deleteNote = (idx) => {
     const newNoteArray = [...noteArray];
-
-    newNoteArray.push({ heading, note });
-    
+    newNoteArray.splice(idx, 1);
     setNoteArray(newNoteArray);
-    console.log(noteArray);
-
-    setHeading("");
-    setNote("");
   };
 
   return (
@@ -39,7 +47,7 @@ const App = () => {
           className="border-2 px-5 py-4 text-xl outline-none rounded"
         />
         <textarea
-          placeholder="write note"
+          placeholder="write note..."
           value={note}
           onChange={(e) => {
             setNote(e.target.value);
@@ -57,22 +65,34 @@ const App = () => {
         </h1>
         <div
           id="notesContainer"
-          className="flex gap-8 pl-8  justify-evenly flex-nowrap lg:flex-wrap h-full overflow-auto lg:border-l-2"
+          className="flex gap-8 pl-8 items-start justify-start flex-nowrap lg:flex-wrap h-[95%] overflow-auto lg:border-l-2 "
         >
-          <div className="min-h-52 min-w-40 bg-white rounded-2xl"></div>
-          <div className="min-h-52 min-w-40 bg-white rounded-2xl"></div>
-          <div className="min-h-52 min-w-40 bg-white rounded-2xl"></div>
-          <div className="min-h-52 min-w-40 bg-white rounded-2xl"></div>
-          <div className="min-h-52 min-w-40 bg-white rounded-2xl"></div>
-          <div className="min-h-52 min-w-40 bg-white rounded-2xl"></div>
-          <div className="min-h-52 min-w-40 bg-white rounded-2xl"></div>
-          <div className="min-h-52 min-w-40 bg-white rounded-2xl"></div>
-          <div className="min-h-52 min-w-40 bg-white rounded-2xl"></div>
-          <div className="min-h-52 min-w-40 bg-white rounded-2xl"></div>
-          <div className="min-h-52 min-w-40 bg-white rounded-2xl"></div>
-          <div className="min-h-52 min-w-40 bg-white rounded-2xl"></div>
-          <div className="min-h-52 min-w-40 bg-white rounded-2xl"></div>
-          <div className="min-h-52 min-w-40 bg-white rounded-2xl"></div>
+          {noteArray.map((el, idx) => {
+            return (
+              <div
+                key={idx}
+                className="relative flex flex-col gap-3 text-black px-6 pt-6 pb-4 h-52 min-w-40 lg:max-h-52 lg:max-w-40 rounded-2xl bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')] bg-cover"
+              >
+                <h3 className="leading-tight font-bold text-xl">
+                  {el.heading}
+                </h3>
+                <p
+                  id="noteText"
+                  className="h-[60%] overflow-auto leading-tight font-medium text-gray-500"
+                >
+                  {el.note}
+                </p>
+                <button
+                  onClick={() => {
+                    deleteNote(idx);
+                  }}
+                  className="bg-red-500 text-sm text-white py-1 w-full rounded-full cursor-pointer active:scale-95"
+                >
+                  Delete
+                </button>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
