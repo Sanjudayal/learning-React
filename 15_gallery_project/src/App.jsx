b4f2f1dd38eba673dade291d104aa267e3af46fd
Dev <sanjudayal999@gmail.com>
2026-09-15 10:55:39 +0530

    gallery project: implement pagination

--- 15_gallery_project/src/App.jsx
+++ 15_gallery_project/src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios, { Axios } from "axios";
+import Card from "./components/Card";
 
 const App = () => {
   const [imageData, setImageData] = useState([]);
+  const [page, setPage] = useState(1);
 
   const getImages = async () => {
     const response = await axios.get(
-      "https://picsum.photos/v2/list?page=4&limit=20",
+      `https://picsum.photos/v2/list?page=${page}&limit=10`,
     );
 
     setImageData(response.data);
@@ -14,33 +16,58 @@ const App = () => {
 
   useEffect(() => {
     getImages();
-  }, []);
+  }, [page]);
 
-  let displayUserImages = "No image available";
+  let displayUserImages = (
+    <h3 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      Loading...
+    </h3>
+  );
 
   if (imageData.length > 0) {
     displayUserImages = imageData.map((el, idx) => {
       return (
         <div key={idx}>
-          <a href={el.url} target="blank">
-            <div key={idx} className="h-40 w-44 bg-white rounded-xl">
-              <img
-                className="h-full w-full object-cover rounded-xl"
-                key={idx}
-                src={el.download_url}
-                alt="image"
-              />
-            </div>
-            <h3 className="font-bold text-lg">{el.author}</h3>
-          </a>
+          <Card el={el} />
         </div>
       );
     });
   }
 
   return (
-    <div className="bg-black overflow-auto text-white h-screen p-5 ">
-      <div className="flex flex-wrap gap-4">{displayUserImages}</div>
+    <div className="bg-black text-white h-screen px-10 pt-10 ">
+      <div
+        id="cardContainer"
+        className="flex flex-wrap justify-center align-center gap-4 h-[83%] overflow-auto"
+      >
+        {displayUserImages}
+      </div>
+      <div className="flex justify-center items-center gap-3 lg:gap-6 px-4 py-6">
+        <button
+          style={{ opacity: page == 1 ? 0.6 : 1 }}
+          className="bg-amber-400 text-black text-sm font-semibold px-5 py-2 rounded cursor-pointer active:scale-95"
+          onClick={() => {
+            if (page > 1) {
+              setImageData([]);
+              setPage(page - 1);
+            }
+            console.log(page);
+          }}
+        >
+          prev
+        </button>
+        <h4 className="font-semibold">Page {page}</h4>
+        <button
+          className="bg-amber-400 text-black text-sm font-semibold px-5 py-2 rounded cursor-pointer active:scale-95"
+          onClick={() => {
+            setImageData([]);
+            setPage(page + 1);
+            console.log(page);
+          }}
+        >
+          next
+        </button>
+      </div>
     </div>
   );
 };
---
+++ 15_gallery_project/src/components/Card.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Card = (props) => {
+  return (
+    <div>
+      {" "}
+      <a href={props.el.url} target="blank">
+        <div key={props.id} className="h-50 w-50 bg-white rounded-xl">
+          <img
+            className="h-full w-full object-cover rounded-xl"
+            key={props.id}
+            src={props.el.download_url}
+            alt="image"
+          />
+        </div>
+        <h3 className="font-bold text-lg">{props.el.author}</h3>
+      </a>
+    </div>
+  );
+};
+
+export default Card;
